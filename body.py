918376b2f32341a708e6666e795e1dcc03570a89
DillonGrabskiInquiries <sahasrahlainquiries@gmail.com>
2021-06-15 00:43:01 -0500

    1

---
+++ body.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def renderLists(name, location, language, comment):
+    users = [
+    {'name' : 'name'},
+    {'dojo_location' : 'location'},
+    {'favorite_language' : 'language'},
+    {'comment' : 'comment'}
+]
+    return render_template("index2.html", userDoc = users)
+
+if __name__=="__main__":
+    app.run(debug=True)
