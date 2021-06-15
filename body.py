35a16e249dedf2332c461de983ac48fe0eb098d6
DillonGrabskiInquiries <sahasrahlainquiries@gmail.com>
2021-06-15 00:43:55 -0500

    1

--- body.py
+++ body.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/result")
 def renderLists(name, location, language, comment):
     users = [
     {'name' : 'name'},
